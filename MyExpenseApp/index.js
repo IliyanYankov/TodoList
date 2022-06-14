251dad82bb7016def4b9c4241f3248287576046f
Todor Markov <todorator@gmail.com>
2022-06-14 22:41:50 +0300

    this is how you save to localstorage

--- MyExpenseApp/index.js
+++ MyExpenseApp/index.js
@@ -29,7 +29,6 @@ const button = document.querySelector(".submit");
 const moneyIncome = document.querySelector(".moneyIncome");
 const totalIncome = document.querySelector(".totalMoney");
 const spendIncome = document.querySelector(".spendMoney");
-let array = [];
 const bank = new Bank(incomeInput, spendInput, placeInput);
 
 const nameUser = window.prompt("Enter name");
@@ -38,69 +37,19 @@ console.log(nameUser);
 button.addEventListener("click", () => {
   bank.show();
   bank.clearFieds();
-  array.push(
-    totalIncome.textContent,
-    moneyIncome.textContent,
-    spendIncome.textContent
-  );
-  console.log(array);
+  username = nameUser
+  state = {
+    totalIncome: totalIncome.textContent,
+    moneyIncome: moneyIncome.textContent,
+    spendIncome: spendIncome.textContent,
+  }
+  storage_state = localStorage.getItem('all_state')
+  if (storage_state === null) {
+    localStorage.setItem('all_state', JSON.stringify({}))
+  } 
+  storage_state = JSON.parse(localStorage.getItem('all_state'))
+
+  storage_state[nameUser] = state
+  localStorage.setItem('all_state', JSON.stringify(storage_state))
+  console.log(localStorage.getItem('all_state'));
 });
-
-let arrayPerson = ["Iliyan", "Ema", "Neli"];
-
-arrayPerson.forEach((person, index) => {
-  let array;
-  console.log(index);
-  if (localStorage.getItem(`${person[index]}`) === null) {
-    array = [];
-  } else {
-    array = JSON.parse(localStorage.getItem(`${person[index]}`));
-  }
-  array.push(new Bank(incomeInput, spendIncome, placeInput));
-});
-
-function getFromLocalStorage() {
-  let array;
-
-  if (localStorage.getItem(`${nameUser}`) === null) {
-    array = [];
-  } else {
-    array = JSON.parse(localStorage.getItem(`${nameUser}`));
-  }
-}
-
-function saveLocalStorage(e) {
-    e.preventDefault();
-    let arr;
-    if (localStorage.getItem("login") === null) {
-      arr = [];
-    } else {
-      arr = JSON.parse(localStorage.getItem("login"));
-    }
-    arr.push(nameReg.value, passwordReg.value, emailReg.value, numberReg.value);
-    localStorage.setItem("login", JSON.stringify(arr));
-  }
-
-const validationLocalStorage = () => {
-    let passwordLog = false;
-    let nameLog = false;
-    let arr;
-
-    if (localStorage.getItem("login") === null) {
-      arr = [];
-    } else {
-      arr = JSON.parse(localStorage.getItem("login"));
-    }
-
-    for (let i = 0; i < arr.length; i++) {
-      if (nameInputLogin.value === arr[i]) {
-        passwordLog = true;
-      }
-      if (passwordLoginInput.value === arr[i]) {
-        nameLog = true;
-      }
-    }
-    if (passwordLog === true && nameLog === true) {
-      return true;
-    }
-  };
